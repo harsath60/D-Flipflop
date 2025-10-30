e44e73ef76beff068ac29c04e564a453de7484fe
harsath60 <sivanesanarasath@gmail.com>
2025-10-30 09:22:55 +0530

    Add files via upload

---
+++ D_Flipflop.v
@@ -0,0 +1,11 @@
+module D_Flipflop( 
+    input  wire clk, rst, D, 
+    output reg  Q 
+); 
+    always @(posedge clk or posedge rst) begin 
+        if (rst) 
+            Q <= 1'b0;   // Reset 
+        else 
+            Q <= D;      // Capture D at clock edge 
+    end 
+endmodule
